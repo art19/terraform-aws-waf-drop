f501ca15adc460fb54b85c67fb122146cb4ca365
Duncan Godfrey <duncan@auth0.com>
2017-03-06 11:48:02 -0500

    Moving ipset and rule creation to native terraform

--- ipsets_and_rules.tf
+++ ipsets_and_rules.tf
@@ -1,21 +1,37 @@
-# Get the CloudFormation stack template by building a Terraform template. #someta
-data "template_file" "waf-ipsets-and-rules" {
-  template = "${file("${path.module}/templates/ipsets_and_rules_cftemplate.json.tpl")}"
-
-  vars {
-    name = "${var.name}"
-    metric_name = "${replace(var.name, "/[^A-Za-z0-9]/", "")}"
+resource "aws_waf_ipset" "WAFIPSet1" {
+  name = "WAFIPSet1"
+  ip_set_descriptors {
+    type = "IPV4"
+    value = "10.10.10.10/32"
   }
 }
 
-# Create IPSets and rules. Since Terraform does not support WAF right now, do this via CloudFormation.
-resource "aws_cloudformation_stack" "waf-ipsets-and-rules" {
-  provider = "aws.terraform-aws-waf-drop"
+resource "aws_waf_ipset" "WAFIPSet2" {
+  name = "WAFIPSet2"
+  ip_set_descriptors {
+    type = "IPV4"
+    value = "10.10.10.10/32"
+  }
+}
 
-  name = "${var.name}-waf-drop-ipsets-and-rules"
-  template_body = "${data.template_file.waf-ipsets-and-rules.rendered}"
+resource "aws_waf_rule" "WAFRule1" {
+  depends_on = ["aws_waf_ipset.WAFIPSet1"]
+  name = "WAFRule1"
+  metric_name = "WAFRule1"
+  predicates {
+    data_id = "${aws_waf_ipset.WAFIPSet1.id}"
+    negated = false
+    type = "IPMatch"
+  }
+}
 
-  tags {
-    Name = "${var.name}-waf-drop-ipsets-and-rules"
+resource "aws_waf_rule" "WAFRule2" {
+  depends_on = ["aws_waf_ipset.WAFIPSet2"]
+  name = "WAFRule2"
+  metric_name = "WAFRule2"
+  predicates {
+    data_id = "${aws_waf_ipset.WAFIPSet2.id}"
+    negated = false
+    type = "IPMatch"
   }
 }
